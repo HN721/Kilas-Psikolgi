f0daf37f094a916640c3ee1eac4d9fb91b9286fc
HN721 <119390246+HN721@users.noreply.github.com>
2024-10-30 09:34:35 +0700

    no

--- src/Component/Comments.jsx
+++ src/Component/Comments.jsx
@@ -7,17 +7,12 @@ export default function Comments() {
       <div className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0"></div>
 
       {/* Comment Input */}
-      <div className="flex bg-primary flex-col w-full ">
+      <div className="flex  flex-col w-full ">
         <input
           className="w-full p-2 border border-gray-300 rounded-lg text-sm bg-white focus:outline-none focus:border-blue-500"
           type="text"
           placeholder="Tambahkan komentar..."
         />
-        <div className="flex justify-end mt-2">
-          <button className="px-4 py-1 text-sm font-semibold text-blue-500 hover:text-blue-700">
-            Kirim
-          </button>
-        </div>
       </div>
     </div>
   );
